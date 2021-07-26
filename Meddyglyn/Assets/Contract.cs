a672a9c77cd378a1e2b2909dc9a09ad88d7fc260
DarraghMakesGames <DarraghMakesGames@users.noreply.github.com>
2021-07-26 14:34:23 +0100

    Changed "sell item" script to more generic "place item" script

---
+++ Meddyglyn/Assets/Contract.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card")]
+public class Contract : ScriptableObject
+{
+    public string title;
+    public string giver;
+    public string description;
+
+}
