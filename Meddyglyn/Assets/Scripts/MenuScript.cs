337c521374747766013bae6ba047872f2f153646
DarraghMakesGames <77898063+DarraghMakesGames@users.noreply.github.com>
2021-08-14 15:27:44 +0100

    Added basic main menu, "Play" button activates player

---
+++ Meddyglyn/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject gameUI;
+    [SerializeField] private GameObject playerCamera;
+
+    public void PlayGame()
+    {
+
+        playerCamera.SetActive(true);
+        gameUI.SetActive(true);
+        player.GetComponent<FirstPersonController>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        this.gameObject.SetActive(false);
+    }
+
+}
